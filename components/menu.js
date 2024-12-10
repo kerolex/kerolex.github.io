dcbfdde0bdc4b8f56314c2bfa04c7105697cf79a
axompero-wyvis <a.xompero@qmul.ac.uk>
2024-12-10 18:56:13 +0000

    Updated index, publications with RAM, open science with ICRA 2025, and reviewing

---
+++ components/menu.js
@@ -0,0 +1,17 @@
+// var ddmenuOptions=
+// {
+//     menuId: "menu",
+//     linkIdToMenuHtml: "menuLink",
+//     open: "onmouseover",  //or "onclick"
+//     delay: 90,
+//     speed: 400,
+//     keysNav: true,
+//     singleOpen: false, // new since v2017.3.17
+//     license: "mylicense"
+// };
+
+// var ddmenu = new Ddmenu(ddmenuOptions);
+
+
+
+
