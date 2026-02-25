42a8d252e4706a41095e97dc4c9f148a932c5a77
Alessio Xompero <alessio.xompero@gmail.com>
2026-02-25 09:58:43 +0000

    Fixed menu vis on mobile

--- js/menu.js
+++ js/menu.js
@@ -52,18 +52,6 @@ document.addEventListener("DOMContentLoaded", function () {
 // Optional: Automatically update year
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 
-// var ddmenuOptions=
-// {
-//     menuId: "menu",
-//     linkIdToMenuHtml: "menuLink",
-//     open: "onmouseover",  //or "onclick"
-//     delay: 90,
-//     speed: 400,
-//     keysNav: true,
-//     singleOpen: false, // new since v2017.3.17
-//     license: "mylicense"
-// };
 
-// var ddmenu = new Ddmenu(ddmenuOptions);
 
 
